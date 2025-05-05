2351b56c1f1fda6a0baaf3927a65581c4f830427
Sathish Kumar <117570195+Sathishkumar18-code@users.noreply.github.com>
2025-05-05 11:36:03 +0530

    build commit

---
+++ Frontend/dist/assets/Card-AlK2HaY3.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Be4YmKgC.js";const r=s=>e.jsx("div",{className:`card ${s.className}`,style:s.style,children:s.children});export{r as C};
--- Frontend/dist/assets/Card-DyQMZeqG.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-yqf1X1x0.js";const r=s=>e.jsx("div",{className:`card ${s.className}`,style:s.style,children:s.children});export{r as C};
